9e08473610723febf51c1afae45d7d4767d04740
Eric Wilson <wilson.eric.n@gmail.com>
2015-10-05 19:30:49 -0400

    made #1 executable

--- pe001.py
+++ pe001.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+
+
 def f0(n):
     """Pythonic solution -- easy to read by slow"""
     return sum([k for k in range(n) if k % 3 == 0 or k % 5 == 0])
@@ -7,3 +10,6 @@ def f1(n):
     """No divisibility checks, ~15x faster"""
     return sum(range(0, n, 3)) + sum(range(0, n, 5)) - sum(range(0, n, 15))
 
+
+if __name__ == '__main__':
+    print(f1(1000))
