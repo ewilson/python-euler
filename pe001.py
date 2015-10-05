c648ff7fac1d914265503468409ff746aa27bd15
Eric Wilson <wilson.eric.n@gmail.com>
2015-10-05 19:11:13 -0400

    #1 completed

---
+++ pe001.py
@@ -0,0 +1,9 @@
+def f0(n):
+    """Pythonic solution -- easy to read by slow"""
+    return sum([k for k in range(n) if k % 3 == 0 or k % 5 == 0])
+
+
+def f1(n):
+    """No divisibility checks, ~15x faster"""
+    return sum(range(0, n, 3)) + sum(range(0, n, 5)) - sum(range(0, n, 15))
+
